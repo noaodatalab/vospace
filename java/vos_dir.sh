85de71ecb4a142da95a3cea786f437ad70565dda
Glenn Eychaner <geychaner@mac.com>
2018-09-25 15:37:27 -0700

    Create the directories as well as the VOSpace entries

--- java/vos_dir.sh
+++ java/vos_dir.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then u=$USER; else u=$1; fi
-if [ $# -lt 2 ]; then h=$(hostname -s); else h=$2; fi
 d=`date +%Y-%m-%dT%H:%M:%S%z`
+hn=`hostname -s`
+if [ $# -lt 1 ]; then u=$USER; else u=$1; fi
+if [ $# -lt 2 ]; then h=$hn; else h=$2; fi
+if [ $# -lt 3 ]; then
+    if [ $hn == "dldev" ] then v="/data/vospace/users/"; else v="/dl2/vospace/users/"; fi
+else v=$3; fi
+
+if [ ! -e ${v}/${u}/public ]; then sudo /bin/mkdir -p ${v}/${u}/public; fi
+if [ ! -e ${v}/${u}/tmp ]; then sudo /bin/mkdir -p ${v}/${u}/tmp; fi
+sudo /bin/chmod 775 ${v}/${u}
+sudo /bin/chmod -R 775 ${v}/${u}/public
+sudo /bin/chmod -R 770 ${v}/${u}/tmp
+sudo /bin/chown -R datalab:datalab ${v}/${u}
+
 /bin/sed -e "s/USER/${u}/g" -e "s/DATE/${d}/g" <<VOBASE | /usr/bin/mysql -u dba -pdba -h ${h} vospace_test
 # users/<USER>
 insert into nodes(identifier, type, owner, view, location, creationDate)
