68abdcd40a1d47803eee01a1be92ba5756741c46
Glenn Eychaner <geychaner@mac.com>
2018-09-26 10:12:45 -0700

    Add ignore to the inserts to continue on error.

--- java/vos_dir.sh
+++ java/vos_dir.sh
@@ -17,24 +17,24 @@ sudo /bin/chown -R datalab:datalab ${v}/${u}
 
 /bin/sed -e "s/USER/${u}/g" -e "s/DATE/${d}/g" <<VOBASE | /usr/bin/mysql -u dba -pdba -h ${h} vospace_test
 # users/<USER>
-insert into nodes(identifier, type, owner, view, location, creationDate)
+insert ignore into nodes(identifier, type, owner, view, location, creationDate)
     values('vos://datalab.noao!vospace/USER', 3, 'USER',
     'ivo://ivoa.net/vospace/views/blob', 'file:///data/vospace/users/USER', now());
-insert into properties(identifier, date, groupread, groupwrite)
+insert ignore into properties(identifier, date, groupread, groupwrite)
     values('vos://datalab.noao!vospace/USER', 'DATE', 'USER', 'USER');
 
 # users/<USER>/public
-insert into nodes(identifier, type, owner, view, location, creationDate)
+insert ignore into nodes(identifier, type, owner, view, location, creationDate)
     values('vos://datalab.noao!vospace/USER/public', 3, 'USER',
     'ivo://ivoa.net/vospace/views/blob', 'file:///data/vospace/users/USER/public', now());
-insert into properties(identifier, date, groupread, groupwrite)
+insert ignoreinto properties(identifier, date, groupread, groupwrite)
     values('vos://datalab.noao!vospace/USER/public', 'DATE', 'USER', 'USER');
 
 # users/<USER>/tmp
-insert into nodes(identifier, type, owner, view, location, creationDate)
+insert ignore into nodes(identifier, type, owner, view, location, creationDate)
     values('vos://datalab.noao!vospace/USER/tmp', 3, 'USER',
     'ivo://ivoa.net/vospace/views/blob', 'file:///data/vospace/users/USER/tmp', now());
-insert into properties(identifier, date, groupread, groupwrite)
+insert ignore into properties(identifier, date, groupread, groupwrite)
     values('vos://datalab.noao!vospace/USER/tmp', 'DATE', 'USER', 'USER');
 VOBASE
 echo "$u VOSpace created on $h"
