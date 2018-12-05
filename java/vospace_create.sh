1ee3f10815c68cc04d288e21a729e01d86d3795f
Glenn Eychaner <geychaner@mac.com>
2018-12-04 17:46:32 -0700

    Split the new vospace creation into a separate create and copy script.

--- java/vospace_create.sh
+++ java/vospace_create.sh
@@ -6,7 +6,7 @@ if [ $# -lt 2 ]; then host='localhost'; else host=$2; fi
 P_MYSQL="/usr/bin/mysql -u dba -pdba -h $host"
 
 exd=`dirname $0`
-$P_MYSQL $db < ${exd}/vospace_convert.sql
+$P_MYSQL $db < ${exd}/vospace_create.sql
 
 # Fill the new metaproperties table
 for c in $(echo "select identifier from metaproperties" | $P_MYSQL vospace -N | cat - ${exd}/vos_properties.txt | sort -u); do
@@ -20,15 +20,3 @@ for c in $(echo "select identifier from metaproperties" | $P_MYSQL $db -N | cut 
 done
 prop_create=$prop_create', PRIMARY KEY (`identifier`(767)) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;'
 echo $prop_create | $P_MYSQL $db
-
-count=$(echo "select count(identifier) from properties" | $P_MYSQL vospace -N)
-echo -ne "${count}\r"
-echo "select * from properties" | $P_MYSQL vospace -N | while read -r i; do
-    id=$(echo $i | cut -d' ' -f1)
-    col=$(echo $i | cut -d' ' -f2 | cut -f2 -d'#')
-    val=$(echo $i | cut -d' ' -f3)
-    echo "INSERT INTO properties (identifier,${col}) VALUES ('${id}','${val}') ON DUPLICATE KEY UPDATE ${col}='${val}';" \
-            | $P_MYSQL $db
-    count=$((count-1))
-    echo -ne "${count} \r"
-done
--- java/vospace_create.sql
+++ java/vospace_create.sql
@@ -1,8 +1,20 @@
 --
+-- Drop all the existing tables
+--
+
+DROP TABLE IF EXISTS `properties`;
+DROP TABLE IF EXISTS `capabilities`;
+DROP TABLE IF EXISTS `jobs`;
+DROP TABLE IF EXISTS `listings`;
+DROP TABLE IF EXISTS `metaproperties`;
+DROP TABLE IF EXISTS `results`;
+DROP TABLE IF EXISTS `transfers`;
+DROP TABLE IF EXISTS `nodes`;
+
+--
 -- Table structure for table `capabilities`
 --
 
-DROP TABLE IF EXISTS `capabilities`;
 CREATE TABLE `capabilities` (
   `identifier` varchar(4096) DEFAULT NULL,
   `capability` varchar(128) DEFAULT NULL,
@@ -11,13 +23,10 @@ CREATE TABLE `capabilities` (
   INDEX cap_cap_idx (`capability`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO capabilities SELECT * from vospace.capabilities;
-
 --
 -- Table structure for table `jobs`
 --
 
-DROP TABLE IF EXISTS `jobs`;
 CREATE TABLE `jobs` (
   `identifier` varchar(128) NOT NULL,
   `type` varchar(45) NOT NULL,
@@ -32,13 +41,10 @@ CREATE TABLE `jobs` (
   INDEX job_typ_idx (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO jobs SELECT * from vospace.jobs;
-
 --
 -- Table structure for table `listings`
 --
 
-DROP TABLE IF EXISTS `listings`;
 CREATE TABLE `listings` (
   `token` varchar(128) NOT NULL,
   `offset` int(11) DEFAULT '0',
@@ -48,13 +54,10 @@ CREATE TABLE `listings` (
   PRIMARY KEY (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO listings SELECT * from vospace.listings;
-
 --
 -- Table structure for table `metaproperties`
 --
 
-DROP TABLE IF EXISTS `metaproperties`;
 CREATE TABLE `metaproperties` (
   `identifier` varchar(128) NOT NULL,
   `type` smallint(6) DEFAULT '0',
@@ -62,53 +65,20 @@ CREATE TABLE `metaproperties` (
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- INSERT INTO metaproperties SELECT * from vospace.metaproperties;
-
---
--- Table structure for table `nodes`
---
-
-DROP TABLE IF EXISTS `nodes`;
-CREATE TABLE `nodes` (
-  `identifier` varchar(4096) NOT NULL,
-  `type` tinyint(4) NOT NULL,
-  `view` varchar(128) DEFAULT NULL,
-  `status` smallint(6) DEFAULT '0',
-  `owner` varchar(128) DEFAULT NULL,
-  `location` varchar(4096) DEFAULT NULL,
-  `creationDate` datetime DEFAULT NULL,
-  `lastModificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `node` text,
-  PRIMARY KEY (`identifier`(767)),
-  INDEX nod_typ_idx (`type`),
-  INDEX nod_own_idx (`owner`),
-  INDEX nod_loc_idx (`location`(767))
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO nodes SELECT * from vospace.nodes;
-ALTER TABLE nodes DROP node;
-ALTER TABLE nodes ADD COLUMN `depth` smallint AFTER identifier;
-ALTER TABLE nodes ADD INDEX nod_dep_idx (`depth`);
-UPDATE nodes SET depth = (LENGTH(identifier)-LENGTH(REPLACE(identifier,'/',''))-3);
-
 --
 -- Table structure for table `results`
 --
 
-DROP TABLE IF EXISTS `results`;
 CREATE TABLE `results` (
   `identifier` varchar(128) NOT NULL,
   `details` text,
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO results SELECT * from vospace.results;
-
 --
 -- Table structure for table `transfers`
 --
 
-DROP TABLE IF EXISTS `transfers`;
 CREATE TABLE `transfers` (
   `identifier` int(11) NOT NULL AUTO_INCREMENT,
   `jobid` varchar(128) DEFAULT NULL,
@@ -118,16 +88,23 @@ CREATE TABLE `transfers` (
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO transfers SELECT * from vospace.transfers;
+--
+-- Table structure for table `nodes`
+--
 
---
--- Table structure for table `properties`
---
-DROP TABLE IF EXISTS `properties`;
-/*
-CREATE TABLE `properties` (
-  `identifier` varchar(128) NOT NULL,
-  `property` varchar(128) NOT NULL,
-  `value` varchar(256) DEFAULT NULL
+CREATE TABLE `nodes` (
+  `identifier` varchar(4096) NOT NULL,
+  `depth` tinyint DEFAULT '-1',
+  `type` tinyint(4) NOT NULL,
+  `view` varchar(128) DEFAULT NULL,
+  `status` smallint(6) DEFAULT '0',
+  `owner` varchar(128) DEFAULT NULL,
+  `location` varchar(4096) DEFAULT NULL,
+  `creationDate` datetime DEFAULT NULL,
+  `lastModificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`identifier`(767)),
+  INDEX nod_dep_idx (`depth`),
+  INDEX nod_typ_idx (`type`),
+  INDEX nod_own_idx (`owner`),
+  INDEX nod_loc_idx (`location`(767))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-*/
