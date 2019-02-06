afe20f22b917c7e8fa201822a7e5f88012b46659
Glenn Eychaner <geychaner@mac.com>
2019-02-06 15:11:04 -0700

    Update the script to fit the new schema.

--- java/vos_dir.sh
+++ java/vos_dir.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
 
-# Arguments are <username>, <hostname>, <vospace_root>
+# Arguments are <username>, <hostname>, <database>, <root_dir>
 d=`date +%Y-%m-%dT%H:%M:%S%z`
 hn=`hostname -s`
 if [ $# -lt 1 ]; then u=$USER; else u=$1; fi
 if [ $# -lt 2 ]; then h=$hn; else h=$2; fi
 if [ $# -lt 3 ]; then
-    if [ $hn == "dldev" ]; then v="/data/vospace/users/"; else v="/dl2/vospace/users/"; fi
-else v=$3; fi
+    if [ $h == "dldb1" -o $h == "dldb1.sdm.noao.edu" ]; then
+        db="vospace_prd"
+    elif [ $h == "dltest" -o $h == "dltest.datalab.noao.edu" ]; then
+        db="vospace_test"
+    else
+        db="vospace_dev"
+    fi
+else db=$3; fi
+if [ $# -lt 4 ]; then
+    if [ $h == "dldb1" -o $h == "dldb1.sdm.noao.edu" ]; then
+        v="/dl2/vospace/users"
+    else
+        v="/data/vospace/users"
+    fi
+else v=$4; fi
 
-if [ ! -e ${v}/${u}/public ]; then sudo /bin/mkdir -p ${v}/${u}/public; fi
-if [ ! -e ${v}/${u}/tmp ]; then sudo /bin/mkdir -p ${v}/${u}/tmp; fi
-sudo /bin/chmod 775 ${v}/${u}
-sudo /bin/chmod -R 775 ${v}/${u}/public
-sudo /bin/chmod -R 770 ${v}/${u}/tmp
-sudo /bin/chown -R datalab:datalab ${v}/${u}
+# Make sure the directories actually exist
+if [ ! -e ${v}/${u} ]; then
+    sudo /bin/mkdir -p ${v}/${u}
+    sudo /bin/chmod 775 ${v}/${u}
+    sudo /bin/chown datalab:datalab ${v}/${u}
+fi
+if [ ! -e ${v}/${u}/public ]; then
+    sudo /bin/mkdir -p ${v}/${u}/public
+    sudo /bin/chmod 775 ${v}/${u}/public
+    sudo /bin/chown datalab:datalab ${v}/${u}/public
+fi
+if [ ! -e ${v}/${u}/tmp ]; then
+    sudo /bin/mkdir -p ${v}/${u}/tmp
+    sudo /bin/chmod 770 ${v}/${u}/tmp
+    sudo /bin/chown datalab:datalab ${v}/${u}/tmp
+fi
 
-/bin/sed -e "s/USER/${u}/g" -e "s/DATE/${d}/g" <<VOBASE | /usr/bin/mysql -u dba -pdba -h ${h} vospace_test
+/bin/sed -e "s/USER/${u}/g" -e "s/DATE/${d}/g" -e "s|ROOT|${v}|g" <<VOBASE | /usr/bin/mysql -u dba -pdba -h ${h} ${db}
 # users/<USER>
-insert ignore into nodes(identifier, type, owner, view, location, creationDate)
-    values('vos://datalab.noao!vospace/USER', 3, 'USER',
-    'ivo://ivoa.net/vospace/views/blob', 'file:///data/vospace/users/USER', now());
+insert ignore into nodes(identifier, depth, type, owner, view, location, creationDate)
+    values('vos://datalab.noao!vospace/USER', 0, 3, 'USER',
+    'ivo://ivoa.net/vospace/views/blob', 'file://ROOT/USER', now());
 insert ignore into properties(identifier, date, groupread, groupwrite)
     values('vos://datalab.noao!vospace/USER', 'DATE', 'USER', 'USER');
 
 # users/<USER>/public
-insert ignore into nodes(identifier, type, owner, view, location, creationDate)
-    values('vos://datalab.noao!vospace/USER/public', 3, 'USER',
-    'ivo://ivoa.net/vospace/views/blob', 'file:///data/vospace/users/USER/public', now());
+insert ignore into nodes(identifier, depth, type, owner, view, location, creationDate)
+    values('vos://datalab.noao!vospace/USER/public', 1, 3, 'USER',
+    'ivo://ivoa.net/vospace/views/blob', 'file://ROOT/USER/public', now());
 insert ignore into properties(identifier, date, groupread, groupwrite)
     values('vos://datalab.noao!vospace/USER/public', 'DATE', 'USER', 'USER');
 
 # users/<USER>/tmp
-insert ignore into nodes(identifier, type, owner, view, location, creationDate)
-    values('vos://datalab.noao!vospace/USER/tmp', 3, 'USER',
-    'ivo://ivoa.net/vospace/views/blob', 'file:///data/vospace/users/USER/tmp', now());
+insert ignore into nodes(identifier, depth, type, owner, view, location, creationDate)
+    values('vos://datalab.noao!vospace/USER/tmp', 1, 3, 'USER',
+    'ivo://ivoa.net/vospace/views/blob', 'file://ROOT/USER/tmp', now());
 insert ignore into properties(identifier, date, groupread, groupwrite)
     values('vos://datalab.noao!vospace/USER/tmp', 'DATE', 'USER', 'USER');
 VOBASE
