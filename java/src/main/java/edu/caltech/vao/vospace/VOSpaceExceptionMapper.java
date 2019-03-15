82c8ece542ffcf33256b72edcce3a09cff458d4c
Glenn Eychaner <geychaner@mac.com>
2019-03-15 14:31:22 -0700

    Remove the println from the exception mapper.

--- java/src/main/java/edu/caltech/vao/vospace/VOSpaceExceptionMapper.java
+++ java/src/main/java/edu/caltech/vao/vospace/VOSpaceExceptionMapper.java
@@ -12,8 +12,8 @@ import javax.ws.rs.ext.Provider;
 public class VOSpaceExceptionMapper implements ExceptionMapper<VOSpaceException> {
 
     public Response toResponse(VOSpaceException e) {
-        System.out.println(e.getStatusCode() + " " + e.getFault());
-        if (e.getIdentifier() != null) System.out.println(e.getIdentifier());
+        // System.out.println(e.getStatusCode() + " " + e.getFault());
+        // if (e.getIdentifier() != null) System.out.println(e.getIdentifier());
         return Response.status(e.getStatusCode()).entity(e.getMessage()).header(
                 "X-VO-Fault",e.getFault()).header("X-VO-Identifier",e.getIdentifier()).build();
     }
