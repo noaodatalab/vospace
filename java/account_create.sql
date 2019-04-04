8fd6c5a80059e0e916d91894410a52ab793d13d1
Glenn Eychaner <geychaner@mac.com>
2019-04-04 16:26:27 -0700

    Create a MySQL container that contains a fully-fledged VOSpace schema.

---
+++ java/account_create.sql
@@ -0,0 +1,3 @@
+create user 'dba' identified by PASSWORD '*381AD08BBFA647B14C82AC1094A29AD4D7E4F51D';
+grant all privileges on vospace.* TO 'dba'@'%' IDENTIFIED BY PASSWORD '*381AD08BBFA647B14C82AC1094A29AD4D7E4F51D';
+create database vospace;
