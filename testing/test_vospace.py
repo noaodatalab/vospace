0539c33ed71931d88931588f7bc26090dca0f37b
chaduhduh <chadd.myers3@gmail.com>
2024-08-28 14:29:10 -0700

    adds additional test specs covering get, create, delete, link and setProp

--- testing/test_vospace.py
+++ testing/test_vospace.py
@@ -3,94 +3,140 @@
 Test VOSpace API interface
 """
 import os
-import time
 import unittest
 import requests
+from testing.utils import (
+    VosHTTP,
+    VosFile,
+    TestUser,
+    VosXML,
+    NodeProperty
+)
 
+# Register various access details for the service.
 SERVICE_PORT = os.environ.get("PUBLISH_PORT", "3002")
 SERVICE_ORIGIN = f"http://127.0.0.1:{SERVICE_PORT}"
+SERVICE_URL = f"{SERVICE_ORIGIN}/vospace-2.0/vospace"
 
-class TestUser:
-    """
-    Represents a file service user for testing purposes
-    """
-    def __init__(self, username: str):
-        self.name = username
-        self.token = f"${self.name}.123.123.faketoken"
+# Initialize our test API client
+REST = VosHTTP(SERVICE_URL)
 
-class VosXML:
-    """
-    Provides various methods for generating various VO XML resources
-    """
-    @staticmethod
-    def container_node(user: str, dir: str):
-        """
-        Return the XML to generate a new container node
-        """
-        std_namespace = "http://www.w3.org/2001/XMLSchema-instance"
-        namespace = "http://www.ivoa.net/xml/VOSpace/v2.0"
-        schema_type = "vos:ContainerNode"
-        base_uri = "vos://datalab.noirlab!vospace"
-        uri = f"{base_uri}/{user}/{dir}"
+# this creates a directory for all of our test data to go in, this keeps the
+# data nested and out of the way
+ROOT_TEST_DIR = f"tests-auto-{VosFile.key()}"
+REST.create_container(TestUser("userone"), ROOT_TEST_DIR)
 
-        return (
-            f"<node \
-                xmlns:xsi='{std_namespace}' \
-                xmlns='{namespace}' \
-                xsi:type='{schema_type}' \
-                uri='{uri}'>\
-                <properties/><accepts/><provides/><capabilities/><nodes/>\
-            </node>"
-        )
 
 class VOSpace(unittest.TestCase):
     """
-    Test VOSpace endpoints
+    Test VOSpace API endpoints
     """
     def setUp(self):
         """
         These steps get performed before each test
         """
-        self.base = f"{SERVICE_ORIGIN}/vospace-2.0/vospace"
+        self.base = SERVICE_URL
         self.user1 = TestUser("userone")
         self.user2 = TestUser("usertwo")
+        self.testdir = f"{ROOT_TEST_DIR}/test-{VosFile.key()}"
+
+        # create a container as our working directory for testing
+        REST.create_container(self.user1, self.testdir)
+
+    def default_params(self, token: str):
+        """
+        Return default headers for VOSpace request
+        """
+        return dict(
+            headers={
+                'Content-Type': 'application/xml',
+                'X-DL-AuthToken': token
+            },
+            timeout=5000
+        )
 
     def test_availability(self):
         """
         Simple test call to discovery service to make sure it is available
         """
-        res = requests.get(self.base, timeout=5000)
+        res = requests.get(SERVICE_URL, timeout=5000)
         self.assertEqual(res.status_code, 200)
-    
+
     def test_get_node(self):
-        pass
+        """
+        Retrieve our main test container
+        """
+        node_url = f"{SERVICE_URL}/nodes/{self.user1.name}"
+        args = self.default_params(self.user1.token)
+
+        # first check a valid node
+        res = requests.get(f"{node_url}/{self.testdir}", **args)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn("vos:ContainerNode", res.text)
+
+        # next make sure a non existent node throws a 404
+        res = requests.get(f"{node_url}/some--missing--dir", **args)
+        self.assertEqual(res.status_code, 404)
+        self.assertNotIn("vos:ContainerNode", res.text)
 
     def test_create_node(self):
-        #TODO: this time based filename is a bit of a stopgap until we have
-        # mechanisms for proper cleanup. Ideally at the  beginning of the test
-        # suite we should cleanup from previous test runs
-        new_dir = f"testdir-auto-{time.strftime('%Y%m%d-%H%M%S')}"
+        """
+        Test creating a new container node
+        """
+        new_dir = f"{self.testdir}/test-{VosFile.key()}"
         node_xml = VosXML.container_node(self.user1.name, new_dir)
         res = requests.put(
-            f"{self.base}/nodes/{self.user1.name}/{new_dir}",
+            f"{SERVICE_URL}/nodes/{self.user1.name}/{new_dir}",
             data=node_xml,
-            headers={
-                'Content-Type': 'application/xml',
-                'X-DL-AuthToken': self.user1.token
-            },
-            timeout=5000
+            **self.default_params(self.user1.token)
             )
         self.assertEqual(res.status_code, 201)
         self.assertIn("vos:ContainerNode", res.text)
 
     def test_delete_node(self):
+        """
+        Test node deletion
+        """
+        # first create a node that we can delete
+        del_testdir = f"{self.testdir}/del-test-{VosFile.key()}"
+        REST.create_container(self.user1, del_testdir)
+        node_url = f"{SERVICE_URL}/nodes/{self.user1.name}"
+        args = self.default_params(self.user1.token)
+
+        # now delete the node we created
+        res = requests.delete(f"{node_url}/{del_testdir}", **args)
+        self.assertEqual(res.status_code, 204)
+
+        # next make sure deleting a non existent node throws an error
+        res = requests.get(f"{node_url}/some--missing--dir", **args)
+        self.assertEqual(res.status_code, 404)
+
+    def test_transfer(self):
         pass
 
     def test_copy_node(self):
         pass
 
     def test_link_node(self):
-        pass
+        # first create a node that we can link later
+        fro_dir = f"{self.testdir}/ln-target-{VosFile.key()}"
+        REST.create_container(self.user1, fro_dir)
+
+        # now create the link
+        to_dir = f"{self.testdir}/ln-{VosFile.key()}"
+        node_xml = VosXML.link_node(self.user1.name, fro_dir, to_dir)
+        res = requests.put(
+            f"{SERVICE_URL}/nodes/{self.user1.name}/{to_dir}",
+            data=node_xml,
+            **self.default_params(self.user1.token)
+            )
+
+        # ensure the request completed and that the link contains a reference
+        # to the target URI
+        self.assertEqual(res.status_code, 201)
+        to_xml = REST.get_node(self.user1, to_dir)
+        self.assertIn("vos:LinkNode", to_xml)
+        self.assertIn(fro_dir, to_xml)
 
     def test_lock_node(self):
         pass
@@ -99,7 +145,42 @@ class VOSpace(unittest.TestCase):
         pass
 
     def test_set_property(self):
-        pass
+        """
+        Test setting properties on existing nodes
+        """
+        # configure various settings
+        prop = NodeProperty(key="TAG", value="testing_new_tag")
+        req_params = self.default_params(self.user1.token)
+        testdir = f"{self.testdir}/test-{VosFile.key()}"
+        node_url = f"{SERVICE_URL}/nodes/{self.user1.name}/{testdir}"
+
+        # first we'll create a node to use and make sure it doesnt have the
+        # target property
+        REST.create_container(self.user1, testdir)
+        self.assertNotIn(prop.value, REST.get_node(self.user1, testdir))
+
+        # after the container is created use the same container XML with an
+        # additional property to update
+        new_node = VosXML.container_node(self.user1.name, testdir, [ prop ])
+        res = requests.post(
+            node_url,
+            data=new_node,
+            **req_params
+            )
+        # make sure the request succeeded and the tag exists
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(prop.value, REST.get_node(self.user1, testdir))
+
+        # Change the property one more time and make sure it persists
+        nprop = NodeProperty(key=prop.key, value="updated_tag")
+        new_node = VosXML.container_node(self.user1.name, testdir, [ nprop ])
+        res = requests.post(
+            node_url,
+            data=new_node,
+            **req_params
+            )
+        self.assertEqual(res.status_code, 200)
+        self.assertIn(nprop.value, REST.get_node(self.user1, testdir))
 
     def get_node_type(self):
         pass
